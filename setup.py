19081f3c1626a53592a0d055d10b3df769f1525a
Justin Azoff <justin@bouncybouncy.net>
2006-08-21 00:18:32 +0000

    try and fix setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,6 @@ from setuptools import setup, find_packages
 setup(
     name = "pyrt",
     version = "0.1",
-    packages = find_packages(),
+    packages = find_packages('.'),
+    package_dir = {'': '.'},
 )
