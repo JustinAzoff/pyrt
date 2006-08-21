e25e48b5421abeb0819e5f85f43a50dbfdc4d3bf
Justin Azoff <justin@bouncybouncy.net>
2006-08-21 00:03:05 +0000

    initial python request tracker client

---
+++ __init__.py
@@ -0,0 +1,47 @@
+import urllib, urllib2, urlparse
+import simplejson
+
+import cookielib
+REST_VERSION = "1.0"
+
+from ticket import *
+
+class RTClient:
+    def __init__(self, url, username, password):
+        self.url = url
+        self.username = username
+        self.password = password
+        
+        self.cj = cookielib.CookieJar()
+        self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cj))
+
+        self.login()
+
+    def _make_url(self, action):
+        return "%s/REST/%s/%s" % (self.url, REST_VERSION, action)
+
+    def _do(self, action, **args):
+        """Call url with args as query args and return the result"""
+
+        for k,v in args.items():
+            if k.endswith("_"):
+                del args[k]
+                args[k[:-1]]=v
+        
+        url  = self._make_url(action)
+        data = urllib.urlencode(args)
+        res  = self.opener.open(url, data).read()
+
+        return res
+
+    def split_res(self, res):
+        ret = res.splitlines()
+        return ret[2:]
+
+    def login(self):
+        self._do('/') #need this to start the session first? O.o
+        return self._do('/', user=self.username, pass_=self.password)
+
+    def _get_ticket(self):
+        return Ticket(self)
+    ticket = property(_get_ticket)
---
+++ ticket.py
@@ -0,0 +1,26 @@
+class Ticket:
+    def __init__(self, rtclient):
+        self.rt = rtclient
+    def search(self, query=None,format=''):
+        page = self.rt._do('search/ticket', query=query, format=format)
+        
+        data = self.rt.split_res(page)
+
+        if format=='i':
+            return data
+
+        if not format:
+            ret = []
+            for line in data:
+                ret.append(line.split(': ', 1))
+            return ret
+ 
+    def find_by_status(self, status=[], **kwargs):
+        q = ' or '.join("status='%s'" % s for s in status)
+        return self.search(query=q, **kwargs)
+
+    def find_open(self, **kwargs):
+        """Find any ticket whose status is new, open, or stalled"""
+        return self.find_by_status(['new','open','stalled'])
+
+__all__ = ["Ticket"]
