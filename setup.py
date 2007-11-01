e37205b596d43e70652483d601beb454f4123146
Justin Azoff <justin@bouncybouncy.net>
2007-11-01 16:22:56 +0000

    make a propper package

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
-from setuptools import setup, find_packages
-setup(
-    name = "pyrt",
+from setuptools import setup
+
+setup(name = "pyrt",
     version = "0.1",
-    packages = find_packages('.'),
-    package_dir = {'': '.'},
+    packages = ['pyrt'],
 )
