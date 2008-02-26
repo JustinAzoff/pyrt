649c6ef7f645dfbba3c42431f2759da951298815
Justin Azoff <justin@bouncybouncy.net>
2008-02-26 21:05:35 +0000

    fix up setup and tests

--- setup.py
+++ setup.py
@@ -1,6 +1,26 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import sys, os
 
-setup(name = "pyrt",
-    version = "0.1",
-    packages = ['pyrt'],
-)
+version = '0.1'
+
+setup(name='pyrt',
+      version=version,
+      description="client for Request Tracker",
+      long_description="""\
+pyrt is a client for the request tracker REST interface""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='RT REST',
+      author='Justin Azoff',
+      author_email='JAzoff@uamail.albany.edu',
+      url='',
+      license='LGPL',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
--- tests/test_ticket_creation.py
+++ tests/test_ticket_creation.py
@@ -10,13 +10,17 @@ it was created for testing at """ + TS + """
 I hope this works :-) :-)
 """
 
+import os
+RT_USER = os.getenv("RT_USER")
+RT_PASS = os.getenv("RT_PASS")
+
 def test_search_non_exist():
-    c=RTClient('http://localhost/rt/', 'djf','djfrtpassword')
+    c=RTClient('http://localhost/rt/', RT_USER, RT_PASS)
     res = c.ticket.find_open({'CF.building':"test building", 'CF.jack': 'test jack'},format='l')
     assert bool(res) == False
 
 def test_create_ticket():
-    c=RTClient('http://localhost/rt/', 'djf','djfrtpassword')
+    c=RTClient('http://localhost/rt/', RT_USER, RT_PASS)
     ticket = c.ticket.create(queue='trouble', subject=SUBJ, requestor="justin", Text=TEXT,
             cf={
                 'department': 'test department',
@@ -31,7 +35,7 @@ def test_create_ticket():
     assert i > 100
 
 def test_search_ticket_i_just_made():
-    c=RTClient('http://localhost/rt/', 'djf','djfrtpassword')
+    c=RTClient('http://localhost/rt/', RT_USER, RT_PASS)
     res = c.ticket.find_open({'CF.building':"test building", 'CF.jack': 'test jack'},format='l')
     assert bool(res) == True
 
@@ -45,7 +49,7 @@ def test_search_ticket_i_just_made():
     yield body_case, ticket
 
 def body_case(tick):
-    c=RTClient('http://localhost/rt/', 'djf','djfrtpassword')
+    c=RTClient('http://localhost/rt/', RT_USER, RT_PASS)
     id = tick['id']
     t = c.ticket.get(id)
     attachments = t.attachment_ids()
@@ -55,7 +59,7 @@ def body_case(tick):
 
 
 def test_close_that_ticket():
-    c=RTClient('http://localhost/rt/', 'djf','djfrtpassword')
+    c=RTClient('http://localhost/rt/', RT_USER, RT_PASS)
     res = c.ticket.find_open({'CF.building':"test building", 'CF.jack': 'test jack'},format='l')
     assert bool(res) == True
     ticket = res[0]
@@ -64,6 +68,6 @@ def test_close_that_ticket():
     print t.edit(status='resolved')
 
 def test_search_non_exist_again():
-    c=RTClient('http://localhost/rt/', 'djf','djfrtpassword')
+    c=RTClient('http://localhost/rt/', RT_USER, RT_PASS)
     res = c.ticket.find_open({'CF.building':"test building", 'CF.jack': 'test jack'},format='l')
     assert bool(res) == False
